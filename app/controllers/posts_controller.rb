921df1d5a2339e919fba4ca2676dfe6f016634cc
Hector Mg <hectormgwake@gmail.com>
2018-02-14 10:45:31 -0600

    Add upload functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
   def index
-    @posts = []
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :root
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :meme)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  has_attached_file :meme, styles: { medium: "500x500>", thumb: "200x200>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :meme, content_type: /\Aimage\/.*\z/
 end
